934daf87bee9f89f52554f2806e9b1e65a8d8dc6
Deekshith <deekshithskulal485@gmail.com>
2025-08-08 22:03:44 +0530

    feat: Designed NoteCard

--- app/src/main/java/com/example/notereminderapp/screen/HomeScreen.kt
+++ app/src/main/java/com/example/notereminderapp/screen/HomeScreen.kt
@@ -12,15 +12,26 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -60,12 +71,34 @@ fun HomeScreen() {
             info = "Fix the kitchen light"
         )
     )
+    var search by remember {
+        mutableStateOf("")
+    }
     Box(modifier = Modifier.fillMaxWidth()
         .background(color = MaterialTheme.colorScheme.background))
     {
         Column(modifier = Modifier.fillMaxSize())
         {
-            TopAppBar(title = { Text(text = "Notes") })
+            TopAppBar(title =
+                {
+                    Text(text = "Notes",
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(horizontal = 5.dp, vertical = 10.dp))
+                },
+                actions = {
+                    Icon(imageVector = Icons.Default.ThumbUp,
+                        contentDescription = "Thme")
+                })
+
+            OutlinedTextField(value = search,
+                onValueChange = {
+                    search=it
+                },
+                modifier = Modifier.fillMaxWidth(0.9f)
+                    .padding(10.dp)
+                    .align(Alignment.CenterHorizontally)
+                    )
             LazyVerticalStaggeredGrid(columns = StaggeredGridCells.Fixed(2),
                 modifier = Modifier.fillMaxSize()) {
                 items(sampleNotes){
@@ -87,6 +120,8 @@ fun NoteCard(note: MNote) {
         Column()
         {
             Text(text = note.title,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp))
             Text(text = note.info,
                 modifier = Modifier.padding(10.dp))
